d483db279371840f35810aa3929893e5f2d6f080
SylvKT <contact@sylv.gay>
2023-08-14 03:06:02 -0400

    actually register the item group you dumbass

--- src/main/java/gay/sylv/phonochat/ItemGroups.java
+++ src/main/java/gay/sylv/phonochat/ItemGroups.java
@@ -3,7 +3,12 @@ package gay.sylv.phonochat;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import static gay.sylv.phonochat.PhonochatMod.MOD_ID;
 
 public class ItemGroups implements Initializable {
 	static final ItemGroups INSTANCE = new ItemGroups();
@@ -15,4 +20,9 @@ public class ItemGroups implements Initializable {
 			.build();
 	
 	private ItemGroups() {}
+	
+	@Override
+	public void initialize() {
+		Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "phonochat"), PHONOCHAT);
+	}
 }
