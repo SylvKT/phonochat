1214ffc5e9abf72c3a5e482ffdeedbec045ddcec
SylvKT <contact@sylv.gay>
2023-08-14 03:03:04 -0400

    add displayName to item group

--- src/main/java/gay/sylv/phonochat/ItemGroups.java
+++ src/main/java/gay/sylv/phonochat/ItemGroups.java
@@ -3,11 +3,13 @@ package gay.sylv.phonochat;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 
 public class ItemGroups implements Initializable {
 	static final ItemGroups INSTANCE = new ItemGroups();
 	
 	public static final ItemGroup PHONOCHAT = FabricItemGroup.builder()
+			.displayName(Text.translatable("phonochat.phonochat_group"))
 			.icon(() -> new ItemStack(Items.MICROPHONE))
 			.entries((displayContext, entries) -> entries.add(Items.MICROPHONE))
 			.build();
