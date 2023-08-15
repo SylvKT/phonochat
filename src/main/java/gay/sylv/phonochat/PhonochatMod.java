f402df1b3f9ab592a8edc02be9f629b0e5e778db
SylvKT <contact@sylv.gay>
2023-08-15 14:13:35 -0400

    disable hearing self

--- src/main/java/gay/sylv/phonochat/PhonochatMod.java
+++ src/main/java/gay/sylv/phonochat/PhonochatMod.java
@@ -9,7 +9,7 @@ public class PhonochatMod implements ModInitializer {
 	public static final String MOD_ID = "phonochat";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final double CHAT_RADIUS = 16;
-	public static final boolean HEAR_SELF = true;
+	public static final boolean HEAR_SELF = false;
 
 	@Override
 	public void onInitialize() {
